3455df0cf0c7e3b0f2b8d74dc9fe124801d4594e
Indrajit Ari <indrajit.ari.440@gmail.com>
2023-07-03 01:15:27 +0530

    my first change

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const https = require("https");
+const bodyParser= require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.get("/" , function ( req , res ){
+    res.sendFile(__dirname + "/index.html")
+    
+});
+app.post("/",function(req,res){
+    const query=req.body.cityName;
+    const apiKey="2d298e264aab0850851489d586e70c36";
+    const url="https://api.openweathermap.org/data/2.5/weather?q="+ query +"&units=metric&appid="+apiKey;
+    https.get(url,function(response){
+        console.log("The status code is: " + response.statusCode);
+        response.on("data",function(data){
+            const weatherData = JSON.parse(data);
+            const cityNam =req.body.cityName;
+            const tem = weatherData.main.temp;
+            const weatherDes = weatherData.weather[0].description;
+            const icon= weatherData.weather[0].icon;
+            const imageUrl= "https://openweathermap.org/img/wn/"+ icon +"@2x.png"
+            res.write("<h1 align='center'>The Temperature at "+ cityNam +" is "+ tem + " degree C</h1>")
+            res.write("<center><img src="+ imageUrl +"></center>" )
+            res.write("<p align='center'>The Weather Description of "+cityNam+" is: "+ "<b> <kbd>" + weatherDes + "</kbd></b>" + " </p>")
+            res.send()
+
+        })
+    })
+});
+app.listen(3000,function(){
+    console.log("server started at port 3000.")
+});
