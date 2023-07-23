a9925c90e87b75ce38d30ca1e2ceeb701b5bc81f
Indrajit Ari <97746000+INdrajit88@users.noreply.github.com>
2023-07-24 02:46:55 +0530

    add styles

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ const express = require("express");
 const https = require("https");
 const bodyParser= require("body-parser");
 const config = require("./config.js");
+const date = require(__dirname+"/date.js");
+const ejs = require("ejs");
+
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine","ejs");
+app.use(express.static("public"));
+
 app.get("/" , function ( req , res ){
-    res.sendFile(__dirname + "/index.html")
+    res.render("index");
     
 });
 app.post("/",function(req,res){
@@ -22,13 +28,18 @@ app.post("/",function(req,res){
             const weatherDes = weatherData.weather[0].description;
             const icon= weatherData.weather[0].icon;
             const imageUrl= "https://openweathermap.org/img/wn/"+ icon +"@2x.png"
-            res.write("<h1 align='center'>The Temperature at "+ cityNam +" is "+ tem + " degree C</h1>")
-            res.write("<center><img src="+ imageUrl +"></center>" )
-            res.write("<p align='center'>The Weather Description of "+cityNam+" is: "+ "<b> <kbd>" + weatherDes + "</kbd></b>" + " </p>")
-            res.send()
-
+            const time = date.getDate();
+            // res.write("<h1 align='center'>The Temperature at "+ cityNam +" is "+ tem + " degree C</h1>")
+            // res.write("<center><img src="+ imageUrl +"></center>" )
+            // res.write("<p align='center'>The Weather Description of "+cityNam+" is: "+ "<b> <kbd>" + weatherDes + "</kbd></b>" + " </p>")
+            // res.send()
+                res.render("home",{cityName:cityNam,temperature:tem,weatherDescription:weatherDes,imageUrl:imageUrl,time:time})
         })
     })
+});
+app.get("/weather",function(req,res){
+
+
 });
 app.listen(3000,function(){
     console.log("server started at port 3000.")
---
+++ date.js
@@ -0,0 +1,10 @@
+module.exports.getDate =function getDate(){
+    const today=new Date();
+// const currentDay= today.getDay();
+const options ={
+    weekday : "long",
+    day : "numeric",
+    month: "long"
+};
+return today.toLocaleDateString("en-US",options);// to gate the date
+ }
